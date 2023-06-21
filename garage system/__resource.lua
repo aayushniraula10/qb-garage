1c25d7584551ee9271fd3954cca2c8ca0fdeaddc
Aayush Niraula <89308757+aayushniraula10@users.noreply.github.com>
2023-06-21 17:55:09 +0545

    Add files via upload

---
+++ garage system/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+client_scripts {
+    'client/functions.lua',
+    'client/client.lua',
+    'client/nui.lua',
+    'client/keys.lua',
+}
+
+server_scripts {
+    'config.lua',
+    'server/server.lua',
+    'server/coords.lua',
+    'server/keys.lua',
+}
+
+ui_page 'html/index.html'
+file 'html/index.html'
